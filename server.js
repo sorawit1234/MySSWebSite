72187559277916105d365160cbeb0a46f860ea90
Sorawit Sangnak <sorawit.s@frontiscompany.com>
2021-11-01 02:46:33 +0700

    'testdeploy'

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const path = require('path');
 const app = express();
 app.use(compression())
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/editemp'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/editemp/index.html'));
-});
 
 // Start the app by listening on the default Heroku po

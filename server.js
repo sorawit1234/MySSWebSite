30f3decd9e3e988d218fc777dc1a8d59ba220404
Sorawit Sangnak <sorawit.s@frontiscompany.com>
2021-11-01 12:45:47 +0700

    'testdeploy'

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //Install express server
-// const compression = require('compression')
 const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(compression())
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
 
-// Start the app by listening on the default Heroku po
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/editemp'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/editemp/' }),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);

778403a52fb0a4fd64b9ff0e069b606eae2b0a88
Sorawit Sangnak <sorawit.s@frontiscompany.com>
2021-11-01 13:04:08 +0700

    'testdeploy1111'

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/editemp'));
+app.use(express.static('./src/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: './src/editemp/' }),
+    res.sendFile('index.html', { root: 'src/' }),
 );
 
 // Start the app by listening on the default Heroku port

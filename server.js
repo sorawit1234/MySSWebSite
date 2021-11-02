42867035556c3f554be5890a60b79a3722f3fd04
Sorawit Sangnak <sorawit.s@frontiscompany.com>
2021-11-02 12:37:18 +0700

    change server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/editemp'));
+app.use(express.static('./src/'));
 
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', { root: './dist/' }),
-// );
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: './src/' }),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
